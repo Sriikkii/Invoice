614dbd3a79c0d574c97c06ddba7862516be1100b
Srikaanth <srikaanth7722@gmail.com>
2022-08-31 00:05:28 +0530

    commit idk

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 #gap = ' '*3
 #heading = f"{"item" : 4s} {gap} {"MRP" : 3s} {gap} {"QTY": 3s} {gap} {"Ammount": 7s} 
+from num2words import num2words 
+d = {"1": "card","2":"UPI","3":"food card","4":"cash"}
+for i in d:
+    print(i)
